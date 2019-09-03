1e4391a43f835ebb5dd151edfb99d5bd7e1aff25
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-03 10:33:02 -0300

    Update Popular_TabNFI.sql
    Comentando

--- Popular_TabNFI.sql
+++ Popular_TabNFI.sql
@@ -1,3 +1,5 @@
+-- Populando tabela Nota fiscal item
+
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (1, 1, 9, 5, 3.50);
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (2, 1, 5, 1, 4.30);
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (3, 2, 13, 3, 2.65);
