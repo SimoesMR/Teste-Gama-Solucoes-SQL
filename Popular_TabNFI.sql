45ebc61fd5c60572afa083e4c839c72b05dde86b
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-03 04:14:36 -0300

    Create Popular_TabNFI.sql

---
+++ Popular_TabNFI.sql
@@ -0,0 +1,5 @@
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (1, 1, 9, 5, 3.50);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (2, 1, 5, 1, 4.30);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (3, 2, 13, 3, 2.65);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (4, 2, 18, 2, 28.00);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (5, 3, 1, 3, 6.99);
