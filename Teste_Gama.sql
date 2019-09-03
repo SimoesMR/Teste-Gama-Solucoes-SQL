9913c6be6b29da163c6005cf29eb1402c4aa0aa9
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-03 08:39:11 -0300

    Update Teste_Gama.sql
    Select para retornar valor mais alto do produto e a soma das notas

--- Teste_Gama.sql
+++ Teste_Gama.sql
@@ -5,3 +5,5 @@ SELECT * FROM teste_gama.produto;
 SELECT * FROM teste_gama.nota_fiscal_item;
 
 SELECT Nome FROM PRODUTO WHERE PrecoUnitario = (SELECT MAX(PrecoUnitario) FROM nota_fiscal_item);  -- Verificar
+
+SELECT SUM(PrecoUnitario) FROM nota_fiscal_item;  -- Perguntar para prof. como arrendar para duas casas decimais
