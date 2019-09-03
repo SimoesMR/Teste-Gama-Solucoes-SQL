c448b72a2d1b3c01f8d21ecbf94ed2d74254b1e3
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-03 15:06:38 -0300

    Update Teste_Gama.sql
    Testando outros select

--- Teste_Gama.sql
+++ Teste_Gama.sql
@@ -8,6 +8,18 @@ SELECT * FROM teste_gama.nota_fiscal_item;
 
 SELECT Nome FROM PRODUTO WHERE PrecoUnitario = (SELECT MAX(PrecoUnitario) FROM nota_fiscal_item);  -- Nome do produto com maior valor de venda (considere o valor do item na nota fiscal).
 
-SELECT SUM(PrecoUnitario) as TotalNF FROM nota_fiscal_item;  -- Valor total de todas as notas e o valor total de todas as notas considerando o preço atual do produto.
+SELECT Nome
+FROM PRODUTO PRDT
+INNER JOIN NOTA_FISCAL_ITEM NFI 
+ON NFI.ProdutoId = PRDT.ProdutoID
+where max(NFI.PrecoUnitario);
+
+-- Valor total de todas as notas e o valor total de todas as notas considerando o preço atual do produto.
+SELECT 
+	SUM(nfi.PrecoUnitario) as ValorTotal_NF,
+	SUM(PRDT.PrecoUnitario) as ValorTotal_NFPrcAtual	
+	FROM nota_fiscal_item nfi
+	INNER JOIN PRODUTO PRDT 
+	ON PRDT.ProdutoID = nfi.ProdutoId;
 
 SELECT PrecoUnitario FROM NOTA_FISCAL_ITEM ORDER BY (Quantidade*PrecoUnitario) DESC LIMIT 5;  -- Nome dos 5 produtos mais vendidos (quantidade) do mês atual.
