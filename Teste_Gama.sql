cdcb4f6fdea2c4ff1c15792448daf9e412c8e3cd
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-03 10:25:54 -0300

    Update Teste_Gama.sql

--- Teste_Gama.sql
+++ Teste_Gama.sql
@@ -4,6 +4,8 @@ SELECT * FROM teste_gama.produto;
 
 SELECT * FROM teste_gama.nota_fiscal_item;
 
-SELECT Nome FROM PRODUTO WHERE PrecoUnitario = (SELECT MAX(PrecoUnitario) FROM nota_fiscal_item);  -- Verificar
+SELECT Nome FROM PRODUTO WHERE PrecoUnitario = (SELECT MAX(PrecoUnitario) FROM nota_fiscal_item);  -- Nome do produto com maior valor de venda (considere o valor do item na nota fiscal).
 
-SELECT SUM(PrecoUnitario) FROM nota_fiscal_item;  -- Perguntar para prof. como arrendar para duas casas decimais
+SELECT SUM(PrecoUnitario) as TotalNF FROM nota_fiscal_item;  -- Valor total de todas as notas e o valor total de todas as notas considerando o preço atual do produto.
+
+SELECT PrecoUnitario FROM NOTA_FISCAL_ITEM ORDER BY (Quantidade*PrecoUnitario) DESC LIMIT 5;  -- Nome dos 5 produtos mais vendidos (quantidade) do mês atual.
