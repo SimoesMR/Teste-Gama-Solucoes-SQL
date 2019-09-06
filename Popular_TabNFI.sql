0c6a7e309ae7460c03581a21d1526b7d52cb916a
SimoesMR <SimoesMR@users.noreply.github.com>
2019-09-05 22:57:22 -0300

    Update Popular_TabNFI.sql

--- Popular_TabNFI.sql
+++ Popular_TabNFI.sql
@@ -1,15 +1,15 @@
 -- Populando tabela Nota fiscal item
 
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (1, 1, 9, 6, 3.50);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (2, 1, 5, 1, 4.30);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (3, 2, 13, 3, 2.65);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (4, 2, 18, 5, 28.00);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (5, 3, 1, 3, 6.99);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (6, 4, 6, 5, 2.50);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (7, 5, 20, 1, 32.20);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (8, 5, 17, 1, 5.90);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (9, 6, 20, 4, 32.20);
-INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (10, 21, 20, 4, 32.20);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (2, 4, 5, 1, 4.30);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (3, 5, 13, 3, 2.65);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (4, 8, 18, 5, 28.00);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (5, 10, 1, 3, 6.99);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (6, 12, 6, 5, 2.50);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (7, 14, 20, 1, 32.20);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (8, 18, 17, 1, 5.90);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (9, 19, 20, 4, 32.20);
+INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (10, 20, 20, 4, 32.20);
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (11, 21, 2, 7, 32.20);
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (12, 22, 8, 5, 8.00);
 INSERT INTO NOTA_FISCAL_ITEM (NotaFiscalItemId, NotaFiscalId, ProdutoId, Quantidade, PrecoUnitario) VALUES (13, 22, 12, 8, 5.14);
